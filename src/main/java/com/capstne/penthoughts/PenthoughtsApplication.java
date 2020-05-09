f671f4ee24ea27ebe379a9cc0131ef25b961afb4
Temidayo Oluseun <toluseun@cogeco.com>
2020-05-09 15:23:50 -0400

    Initial commit

---
+++ src/main/java/com/capstne/penthoughts/PenthoughtsApplication.java
@@ -0,0 +1,13 @@
+package com.capstne.penthoughts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PenthoughtsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PenthoughtsApplication.class, args);
+	}
+
+}
