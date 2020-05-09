cbc7ca8293699de4dbcc0744bcc49c99e84d8464
toluseun <temi.oluseun@gmail.com>
2020-05-09 15:46:06 -0400

    Created default route for EntryController.

--- src/main/java/com/capstne/penthoughts/controllers/EntryController.java
+++ src/main/java/com/capstne/penthoughts/controllers/EntryController.java
@@ -1,4 +1,16 @@
 package com.capstne.penthoughts.controllers;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "/entry")
 public class EntryController {
+
+    @GetMapping(path="/", produces = "application/json")
+    public String getAllEntries()
+    {
+        return "Hello!";
+    }
 }
