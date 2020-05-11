01f2cafc6a9bea530d4a22e0bb55faa547f66189
toluseun <temi.oluseun@gmail.com>
2020-05-10 21:12:31 -0400

    Created add entry operation

--- src/main/java/com/capstne/penthoughts/controllers/EntryController.java
+++ src/main/java/com/capstne/penthoughts/controllers/EntryController.java
@@ -2,10 +2,14 @@ package com.capstne.penthoughts.controllers;
 
 import com.capstne.penthoughts.daos.EntryDAO;
 import com.capstne.penthoughts.model.Entries;
+import com.capstne.penthoughts.model.Entry;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.time.LocalDateTime;
 
 @RestController
 @RequestMapping(path = "/entry")
@@ -18,5 +22,21 @@ public class EntryController {
     public Entries getAllEntries()
     {
         return entryDAO.getEntriesList();
+    }
+
+    @PostMapping(path="/", consumes = "application/json", produces = "application/json")
+    public ResponseEntity<Object> addEntry(@RequestBody Entry entry){
+        long id = entryDAO.getEntriesList().getEntryList().size() + 1;
+        entry.setId(id);
+        entry.setCreatedTime(LocalDateTime.now());
+        entry.setUpdatedTime(null);
+
+        entryDAO.addEntry(entry);
+
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(entry.getId())
+                .toUri();
+        return ResponseEntity.created(location).build();
     }
 }
